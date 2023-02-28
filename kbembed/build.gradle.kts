a3dbdfd9c069ffd55dd6fd028c679d3e22d7455b
dariuszszlag <99176959+dariuszszlag@users.noreply.github.com>
2023-02-28 21:44:24 +0100

    Create release for KBEmbed

--- kbembed/build.gradle.kts
+++ kbembed/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     `maven-publish`
 }
 
+group = "com.dariusz"
+version = System.getenv("VERSION_NAME")
+
+val GIT_USER: String? by project
+val GIT_TOKEN: String? by project
+
 android {
     namespace = "com.dariusz.kbembed"
     compileSdk = 33
@@ -68,39 +74,17 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                description = project.extra["library_description"].toString()
-                groupId = project.extra["group_id"].toString()
-                artifactId = project.extra["artifact_id"].toString()
-                version = project.extra["version_name"].toString()
-                artifact("$buildDir/outputs/aar/${artifactId}-release.aar")
-            }
-        }
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/dariuszszlag/KBMultiplatform")
-                credentials {
-                    username = System.getenv("GIT_USER")
-                    password = System.getenv("GIT_TOKEN")
-                }
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/dariuszszlag/KBMultiplatform")
+            credentials {
+                username = GIT_USER
+                password = GIT_TOKEN
             }
         }
     }
-}
-
-mapOf(
-    Pair("group_id", "com.dariusz"),
-    Pair("artifact_id", "kbembed"),
-    Pair("version_code", System.getenv("VERSION_CODE")),
-    Pair("version_name", System.getenv("VERSION_NAME")),
-    Pair("library_name", "KBEmbed"),
-    Pair("library_description", "Embed version of KB")
-).entries.forEach {
-    project.extra.set(it.key, it.value)
 }
 
 tasks.withType<PublishToMavenRepository> {
