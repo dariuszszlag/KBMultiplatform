49799308f6e512ff7a3541809562b292ec15ce0c
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-03-09 17:01:17 +0100

    Fix kbembed

--- kbembed/src/main/java/com/dariusz/kbembed/navigation/Navigator.kt
+++ kbembed/src/main/java/com/dariusz/kbembed/navigation/Navigator.kt
@@ -9,7 +9,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.dariusz.kbcore.KBCore
-import com.dariusz.kbembed.data.KBCoreDataSource
 import com.dariusz.kbembed.ui.screens.drafts.DraftsScreen
 import com.dariusz.kbembed.ui.screens.drafts.DraftsScreenViewModel
 import com.dariusz.kbembed.ui.screens.home.HomeScreen
