e4e1b59883de6298c9ce282ffe9a24160b870302
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-03-09 14:13:06 +0100

    Presetup

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -34,7 +34,25 @@ android {
 }
 
 dependencies {
+    implementation("com.dariusz:kbembed:0.0.30")
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.runtime:runtime")
+    implementation("androidx.compose.runtime:runtime-saveable")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation(project(mapOf("path" to ":kbembed")))
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.0-rc01")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
