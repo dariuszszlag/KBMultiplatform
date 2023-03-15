b520f5d6d41155b0f89facb4a974a194bb4587a3
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-03-15 22:09:45 +0100

    Apply new builder to android app

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
 dependencies {
     implementation("com.dariusz:kbcore-android:0.1.1")
-    implementation("com.dariusz:kbembed:0.1.0")
+    implementation("com.dariusz:kbembed:0.1.10")
     implementation(platform("androidx.compose:compose-bom:2023.01.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
@@ -47,8 +47,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-rc01")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
