31fe5e50bcfb9860c1d5cb2f2476521c65a66aa1
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-16 23:53:20 +0200

    Change checksumstring in package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "0.1.16"
+let version = "0.1.17"
 
 let xcFrameworkUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip"
 let checksumUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip.md5"
 
-let checksumString = Data(contentsOf: Bundle.main.url(forResource: checksumUrl, withExtension:"md5"))
+let checksumString = Data(contentsOf: URL(string: checksumUrl))
 
 let packageName = "kbcore"
 
