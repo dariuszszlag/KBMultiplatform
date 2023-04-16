a6df6728056d4c24cf9c87d0c3992da1fe31d341
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-16 23:20:22 +0200

    Change checksum string

--- Package.swift
+++ Package.swift
@@ -6,14 +6,7 @@ let version = "0.1.16"
 let xcFrameworkUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip"
 let checksumUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip.md5"
 
-do {
-    guard let url = Bundle.main.url(forResource: checksumUrl, withExtension:"md5") else {
-        return
-    }
-    let checksumString = try Data(contentsOf: url)
-} catch {
-    print(error)
-}
+let checksumString = try Data(contentsOf: Bundle.main.url(forResource: checksumUrl, withExtension:"md5"))
 
 let packageName = "kbcore"
 
