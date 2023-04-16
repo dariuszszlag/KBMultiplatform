e00943f9ae8f816ad5f53fff4f48750ee8828c84
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-16 23:21:36 +0200

    Remove try keyword

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let version = "0.1.16"
 let xcFrameworkUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip"
 let checksumUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip.md5"
 
-let checksumString = try Data(contentsOf: Bundle.main.url(forResource: checksumUrl, withExtension:"md5"))
+let checksumString = Data(contentsOf: Bundle.main.url(forResource: checksumUrl, withExtension:"md5"))
 
 let packageName = "kbcore"
 
