fd1bace4420882f37f12b4396809653db82165e2
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-17 22:07:37 +0200

    Add manual version if null

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 import Foundation
 
-let version = ProcessInfo.processInfo.environment["VERSION_NAME"]
+let version = ProcessInfo.processInfo.environment["VERSION_NAME"] ?: "0.1.20"
 
 let xcFrameworkUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(String(describing: version))/kbcore-kmmbridge-\(String(describing: version)).zip"
 
