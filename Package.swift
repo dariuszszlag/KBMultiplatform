287d2d509ebc00d74877ce3e99c70a9bfe246ba4
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-17 21:35:02 +0200

    Improve package swift file

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ import Foundation
 
 let version = ProcessInfo.processInfo.environment["VERSION_NAME"]
 
-let xcFrameworkUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip"
+let xcFrameworkUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(String(describing: version))/kbcore-kmmbridge-\(String(describing: version)).zip"
 
-let checksumUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(version)/kbcore-kmmbridge-\(version).zip.sha256"
+let checksumUrl = "https://maven.pkg.github.com/dariuszszlag/KBMultiplatform/com/dariusz/kbcore-kmmbridge/\(String(describing: version))/kbcore-kmmbridge-\(String(describing: version)).zip.sha256"
 
 let checksumString = "3d484a39d3f2a037cdd7e7ca7a909e034552e03e957a2f20cf923e7a7e0f4719"
 
