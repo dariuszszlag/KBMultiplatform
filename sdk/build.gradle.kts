7906e19864da3aa1be1e74f52a1b20c0b2b34144
Oskar Rek <oskarrek98@gmail.com>
2023-04-20 14:49:34 +0200

    Chore: try to add transitive dependencies to aar

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -45,8 +45,8 @@ android {
 }
 
 dependencies {
-    api("com.dariusz:kbcore-android:0.1.1") { isTransitive = true }
-    api("com.dariusz:kbembed:0.1.10") { isTransitive = true }
+    api("com.dariusz:kbcore-android:0.1.1")
+    api("com.dariusz:kbembed:0.1.10")
     api("androidx.core:core-ktx:1.7.0")
     api("androidx.appcompat:appcompat:1.6.1")
     api("com.google.android.material:material:1.8.0")
@@ -54,6 +54,11 @@ dependencies {
     testApi("junit:junit:4.13.2")
     androidTestApi("androidx.test.ext:junit:1.1.5")
     androidTestApi("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+val sourceJar by tasks.registering(Jar::class) {
+    from(android.sourceSets.getByName("main").java.srcDirs)
+    archiveClassifier.set("sources")
 }
 
 publishing {
@@ -64,6 +69,18 @@ publishing {
             artifactId = "sdk"
             version = System.getenv("VERSION_NAME")
             artifact("$buildDir/outputs/aar/${artifactId}-release.aar")
+            artifact(sourceJar)
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                //Iterate over the compile dependencies (we don't want the test ones), adding a <dependency> node for each
+                configurations.api.get().allDependencies.forEach {
+                    val dependencyNode = dependenciesNode.appendNode ("dependency")
+                    dependencyNode.appendNode("groupId", it.group)
+                    dependencyNode.appendNode("artifactId", it.name)
+                    dependencyNode.appendNode("version", it.version)
+                }
+            }
         }
     }
     repositories {
