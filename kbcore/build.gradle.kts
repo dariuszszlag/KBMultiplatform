5d5eebf6b6b2d52c629ce63a7cba0e61c9dc05bd
Oskar Rek <oskar.rek@appunite.com>
2023-05-09 12:58:02 +0200

    Fix: revert minimalisation for kbcore

--- kbcore/build.gradle.kts
+++ kbcore/build.gradle.kts
@@ -89,7 +89,7 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
