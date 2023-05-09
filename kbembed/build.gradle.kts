f61a0427755e1dd30b24c50b10aa474ac3ca410b
Oskar Rek <oskarrek98@gmail.com>
2023-05-09 12:14:17 +0200

    Chore: set core as generated script

--- kbembed/build.gradle.kts
+++ kbembed/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 }
 
 dependencies {
-    implementation("com.dariusz:kbcore-android:0.1.1")
+    implementation("com.dariusz:kbcore-android:0.1.5")
     implementation(platform("androidx.compose:compose-bom:2023.01.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
